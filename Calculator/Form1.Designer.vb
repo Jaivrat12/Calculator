4b538502fb6aadb200c485574769c7d33e55a0b9
Jaivrat <jaivratdas123456@gmail.com>
2021-03-09 23:15:15 +0530

    Completed

--- Calculator/Form1.Designer.vb
+++ Calculator/Form1.Designer.vb
@@ -24,73 +24,104 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Num1 = New System.Windows.Forms.TextBox()
+        Me.Num2 = New System.Windows.Forms.TextBox()
+        Me.Add_Button = New System.Windows.Forms.Button()
+        Me.Subtract_Button = New System.Windows.Forms.Button()
+        Me.Multiply_Button = New System.Windows.Forms.Button()
+        Me.Divide_Button = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(54, 65)
+        Me.Label1.Location = New System.Drawing.Point(37, 43)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(46, 17)
+        Me.Label1.Size = New System.Drawing.Size(54, 17)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Label1"
+        Me.Label1.Text = "NUM 1:"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(54, 107)
+        Me.Label2.Location = New System.Drawing.Point(37, 85)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(46, 17)
+        Me.Label2.Size = New System.Drawing.Size(54, 17)
         Me.Label2.TabIndex = 1
-        Me.Label2.Text = "Label2"
+        Me.Label2.Text = "NUM 2:"
         '
-        'TextBox1
+        'Num1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(169, 65)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 25)
-        Me.TextBox1.TabIndex = 2
+        Me.Num1.BackColor = System.Drawing.Color.DeepSkyBlue
+        Me.Num1.Location = New System.Drawing.Point(115, 43)
+        Me.Num1.Name = "Num1"
+        Me.Num1.Size = New System.Drawing.Size(167, 24)
+        Me.Num1.TabIndex = 2
         '
-        'TextBox2
+        'Num2
         '
-        Me.TextBox2.Location = New System.Drawing.Point(169, 107)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(100, 25)
-        Me.TextBox2.TabIndex = 3
+        Me.Num2.BackColor = System.Drawing.Color.DeepSkyBlue
+        Me.Num2.Location = New System.Drawing.Point(115, 85)
+        Me.Num2.Name = "Num2"
+        Me.Num2.Size = New System.Drawing.Size(167, 24)
+        Me.Num2.TabIndex = 3
         '
-        'Button1
+        'Add_Button
         '
-        Me.Button1.Location = New System.Drawing.Point(54, 173)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 24)
-        Me.Button1.TabIndex = 4
-        Me.Button1.Text = "Calculate"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.Add_Button.BackColor = System.Drawing.Color.Indigo
+        Me.Add_Button.Location = New System.Drawing.Point(65, 131)
+        Me.Add_Button.Name = "Add_Button"
+        Me.Add_Button.Size = New System.Drawing.Size(84, 34)
+        Me.Add_Button.TabIndex = 4
+        Me.Add_Button.Text = "ADD"
+        Me.Add_Button.UseVisualStyleBackColor = False
         '
-        'TextBox3
+        'Subtract_Button
         '
-        Me.TextBox3.Location = New System.Drawing.Point(169, 172)
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(100, 25)
-        Me.TextBox3.TabIndex = 5
+        Me.Subtract_Button.BackColor = System.Drawing.Color.Indigo
+        Me.Subtract_Button.Location = New System.Drawing.Point(173, 131)
+        Me.Subtract_Button.Name = "Subtract_Button"
+        Me.Subtract_Button.Size = New System.Drawing.Size(83, 34)
+        Me.Subtract_Button.TabIndex = 7
+        Me.Subtract_Button.Text = "SUBTRACT"
+        Me.Subtract_Button.UseVisualStyleBackColor = False
+        '
+        'Multiply_Button
+        '
+        Me.Multiply_Button.BackColor = System.Drawing.Color.Indigo
+        Me.Multiply_Button.Location = New System.Drawing.Point(65, 184)
+        Me.Multiply_Button.Name = "Multiply_Button"
+        Me.Multiply_Button.Size = New System.Drawing.Size(84, 34)
+        Me.Multiply_Button.TabIndex = 8
+        Me.Multiply_Button.Text = "MULTIPLY"
+        Me.Multiply_Button.UseVisualStyleBackColor = False
+        '
+        'Divide_Button
+        '
+        Me.Divide_Button.BackColor = System.Drawing.Color.Indigo
+        Me.Divide_Button.Location = New System.Drawing.Point(173, 184)
+        Me.Divide_Button.Name = "Divide_Button"
+        Me.Divide_Button.Size = New System.Drawing.Size(84, 34)
+        Me.Divide_Button.TabIndex = 9
+        Me.Divide_Button.Text = "DIVIDE"
+        Me.Divide_Button.UseVisualStyleBackColor = False
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 17.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 17.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.HotTrack
-        Me.ClientSize = New System.Drawing.Size(384, 441)
-        Me.Controls.Add(Me.TextBox3)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.TextBox2)
-        Me.Controls.Add(Me.TextBox1)
+        Me.ClientSize = New System.Drawing.Size(321, 250)
+        Me.Controls.Add(Me.Divide_Button)
+        Me.Controls.Add(Me.Multiply_Button)
+        Me.Controls.Add(Me.Subtract_Button)
+        Me.Controls.Add(Me.Add_Button)
+        Me.Controls.Add(Me.Num2)
+        Me.Controls.Add(Me.Num1)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
-        Me.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.5!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.ForeColor = System.Drawing.SystemColors.HighlightText
         Me.Name = "Form1"
         Me.Text = "Calculator 1.0"
         Me.ResumeLayout(False)
@@ -100,8 +131,10 @@ Partial Class Form1
 
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents Button1 As Button
-    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Num1 As TextBox
+    Friend WithEvents Num2 As TextBox
+    Friend WithEvents Add_Button As Button
+    Friend WithEvents Subtract_Button As Button
+    Friend WithEvents Multiply_Button As Button
+    Friend WithEvents Divide_Button As Button
 End Class
--- Calculator/Form1.vb
+++ Calculator/Form1.vb
@@ -1,3 +1,43 @@
 ﻿Public Class Form1
 
+    Private Sub Sum_Click(sender As Object, e As EventArgs) Handles Add_Button.Click
+
+        Dim a, b As Integer
+        a = Num1.Text
+        b = Num2.Text
+
+        MessageBox.Show(a + b)
+
+    End Sub
+
+    Private Sub Subtract_Click(sender As Object, e As EventArgs) Handles Subtract_Button.Click
+
+        Dim a, b As Integer
+        a = Num1.Text
+        b = Num2.Text
+
+        MessageBox.Show(a - b)
+
+    End Sub
+
+    Private Sub Multiply_Click(sender As Object, e As EventArgs) Handles Multiply_Button.Click
+
+        Dim a, b As Integer
+        a = Num1.Text
+        b = Num2.Text
+
+        MessageBox.Show(a * b)
+
+    End Sub
+
+    Private Sub Divide_Click(sender As Object, e As EventArgs) Handles Divide_Button.Click
+
+        Dim a, b As Integer
+        a = Num1.Text
+        b = Num2.Text
+
+        MessageBox.Show(a / b)
+
+    End Sub
+
 End Class
